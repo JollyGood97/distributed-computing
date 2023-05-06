5da480004e0b262f3c1e3100b6244fb40e074a5d
Semini <seminidpera@gmail.com>
2023-05-06 19:10:34 +0530

    Consul

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import actuator from "express-actuator";
+import Consul from "consul";
 
 import { node } from "./NodeInstance.js";
 import * as dotenv from "dotenv";
@@ -30,41 +31,45 @@ let higherNodes = [];
 let ports = [];
 let higherPorts = [];
 let masterId = null;
-// Register with Eureka Server
-const client = new eureka({
-  instance: {
-    instanceId: node.nodeId,
-    app: "password-cracker",
-    hostName: `localhost:${port}`,
-    ipAddr: "127.0.0.1",
-    statusPageUrl: `http://localhost:${process.env.PORT}/actuator/info`,
-    healthCheckUrl: `http://localhost:${process.env.PORT}/actuator/health`,
-    vipAddress: `password-cracker-${port}.com`,
-    dataCenterInfo: {
-      name: "MyOwn",
-      "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
-    },
-    port: {
-      $: Number(port),
-      "@enabled": true,
-    },
 
-    // leaseInfo: {
-    //   renewalIntervalInSecs: 5,
-    //   durationInSecs: 5,
-    //   registrationTimestamp: parseInt(Date()),
-    // },
-  },
-  eureka: {
-    registryFetchInterval: 100,
-    fetchRegistry: true,
-    fetchMetadata: true,
-    host: "localhost",
-    port: 8761,
-    servicePath: "/eureka/apps/",
-    // heartbeatInterval: 1000,
-  },
-});
+const consul = new Consul();
+const serviceName = "password-cracker";
+
+// Register with Eureka Server
+// const client = new eureka({
+//   instance: {
+//     instanceId: node.nodeId,
+//     app: "password-cracker",
+//     hostName: `localhost:${port}`,
+//     ipAddr: "127.0.0.1",
+//     statusPageUrl: `http://localhost:${process.env.PORT}/actuator/info`,
+//     healthCheckUrl: `http://localhost:${process.env.PORT}/actuator/health`,
+//     vipAddress: `password-cracker-${port}.com`,
+//     dataCenterInfo: {
+//       name: "MyOwn",
+//       "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+//     },
+//     port: {
+//       $: Number(port),
+//       "@enabled": true,
+//     },
+
+//     // leaseInfo: {
+//     //   renewalIntervalInSecs: 5,
+//     //   durationInSecs: 5,
+//     //   registrationTimestamp: parseInt(Date()),
+//     // },
+//   },
+//   eureka: {
+//     registryFetchInterval: 100,
+//     fetchRegistry: true,
+//     fetchMetadata: true,
+//     host: "localhost",
+//     port: 8761,
+//     servicePath: "/eureka/apps/",
+//     // heartbeatInterval: 1000,
+//   },
+// });
 
 // async function updateInstances() {
 //   allInstances = client.getInstancesByAppId("password-cracker");
@@ -79,39 +84,53 @@ const client = new eureka({
 //   console.log("allInstancesWithDetails", allInstancesWithDetails);
 // }
 
-client.start(async (error) => {
-  if (error) {
-    console.log("Error starting the Eureka Client");
-    console.log(error);
-    return;
+consul.agent.service.register(
+  {
+    name: serviceName,
+    id: node.nodeId.toString(),
+    address: "localhost",
+    port: Number(port),
+    check: {
+      http: `http://localhost:${port}/actuator/health`,
+      interval: "10s",
+    },
+  },
+  (err) => {
+    if (err) {
+      console.log("Error registering service with Consul");
+      console.log(err);
+      return;
+    } else {
+      console.log(err);
+    }
+
+    // The rest of your logic goes here
   }
-  console.log("Eureka Client started successfully");
+);
 
-  // Wait for a period of time before starting the election process
-  const waitTime = Math.floor(Math.random() * (15000 - 5000) + 5000);
-  console.log(
-    `Waiting for ${
-      waitTime / 1000
-    } seconds before starting nodes discovery on service registry...`
-  );
-  setTimeout(async () => {
-    // Check if an election is ongoing or if a master has already been elected
-    // if (isElectionOngoing || masterHasBeenElected) {
-    //   console.log(
-    //     "An election is already ongoing or a master has already been elected. Exiting 1..."
-    //   );
-    //   return;
-    // }
+// Wait for a period of time before starting the election process
+// const waitTime = Math.floor(Math.random() * (15000 - 5000) + 5000);
+// console.log(
+//   `Waiting for ${
+//     waitTime / 1000
+//   } seconds before starting nodes discovery on service registry...`
+// );
 
-    // Set a flag to indicate that an election is ongoing
+const getServiceNodes = async () => {
+  const services = await consul.agent.service.list();
+  return services;
+};
 
-    // Fetch all the instances of the `password-cracker` application from Eureka
-    const allInstances = client.getInstancesByAppId(`password-cracker`);
+const startFirstPhase = async () => {
+  try {
+    const allInstances = await getServiceNodes();
+
     console.log(`allInstances`, allInstances);
 
-    // Get the ports of all the instances of the `password-cracker` application
-    // @ts-ignore
-    ports = allInstances.map((instance) => instance.port.$);
+    // Get the ports of all the instances
+
+    ports = Object.values(allInstances).map((instance) => instance.Port);
+    console.log(`ports`, ports);
 
     // Get details of all nodes and filter the higher nodes
     allInstancesWithDetails = await getAllNodesDetails(ports);
@@ -138,13 +157,77 @@ client.start(async (error) => {
         // await waitForElection();
         await startElection(node, higherPorts, ports);
       }, electionWaitTime);
-    } else {
-      console.log(
-        "A master has already been elected or an election is ongoing. Exiting 2..."
-      );
     }
-  }, waitTime);
-});
+  } catch (err) {
+    console.log("Error fetching service instances from Consul");
+    console.log(err);
+  }
+};
+
+console.log("wait 5 seconds to discover all instances");
+setTimeout(() => {
+  startFirstPhase();
+}, 5000);
+
+// client.start(async (error) => {
+//   if (error) {
+//     console.log("Error starting the Eureka Client");
+//     console.log(error);
+//     return;
+//   }
+//   console.log("Eureka Client started successfully");
+
+//   setTimeout(async () => {
+//     // Check if an election is ongoing or if a master has already been elected
+//     // if (isElectionOngoing || masterHasBeenElected) {
+//     //   console.log(
+//     //     "An election is already ongoing or a master has already been elected. Exiting 1..."
+//     //   );
+//     //   return;
+//     // }
+
+//     // Set a flag to indicate that an election is ongoing
+
+//     // Fetch all the instances of the `password-cracker` application from Eureka
+//     const allInstances = client.getInstancesByAppId(`password-cracker`);
+//     console.log(`allInstances`, allInstances);
+
+//     // Get the ports of all the instances of the `password-cracker` application
+//     // @ts-ignore
+//     ports = allInstances.map((instance) => instance.port.$);
+
+//     // Get details of all nodes and filter the higher nodes
+//     allInstancesWithDetails = await getAllNodesDetails(ports);
+//     higherNodes = allInstancesWithDetails.filter(
+//       (otherNode) => otherNode.nodeId > node.nodeId
+//     );
+//     higherPorts = higherNodes.map((node) => node.port);
+
+//     // Check if a master has already been elected or if an election is ongoing
+//     masterHasBeenElected = allInstancesWithDetails.some(
+//       (node) => node.isMaster
+//     );
+//     isElectionOngoing = allInstancesWithDetails.some((node) => node.isElection);
+
+//     if (!masterHasBeenElected && !isElectionOngoing) {
+//       // Wait for a random amount of time before starting the election
+//       const electionWaitTime = Math.floor(
+//         Math.random() * (15000 - 5000) + 5000
+//       );
+//       console.log(
+//         `Starting the election in ${electionWaitTime / 1000} seconds!`
+//       );
+//       setTimeout(async () => {
+//         // await waitForElection();
+//         await startElection(node, higherPorts, ports);
+//       }, electionWaitTime);
+//     } else {
+//       console.log(
+//         "A master has already been elected or an election is ongoing. Exiting 2..."
+//       );
+//     }
+//   }, waitTime);
+// });
 
 app.use(bodyParser.json());
 // Routes
@@ -234,21 +317,24 @@ const options = {
 
 app.use(sidecarProxy);
 app.use(actuator(options));
-function gracefulShutdown() {
+
+async function gracefulShutdown() {
   console.log("Shutting down gracefully...");
-  client.stop((err) => {
-    if (err) {
-      console.log("Error stopping Eureka client", err);
-    } else {
-      console.log("Eureka client stopped successfully");
-      process.exit(0);
+  await consul.agent.service.deregister(
+    { id: node.nodeId.toString() },
+    (err) => {
+      console.log("Error de-registering service:", err);
+
+      process.exit();
     }
-  });
+  );
+  console.log("Deregistered");
+  process.exit();
 }
 
 // console.log("Node status after announcement: ", node);
 
-// Handle various exit signals
+//Handle various exit signals
 process.on("SIGINT", gracefulShutdown);
 process.on("SIGTERM", gracefulShutdown);
 process.on("SIGHUP", gracefulShutdown);
